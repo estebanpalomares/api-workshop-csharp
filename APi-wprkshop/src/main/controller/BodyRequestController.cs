61f70e5996b29d0a0df021174768afa0254b1ceb
Jaime Palomares <jaime.palomares+DFIN@dfinsolutions.com>
2023-03-08 10:13:19 -0500

    Methods Scenarios implemented

---
+++ APi-wprkshop/src/main/controller/BodyRequestController.cs
@@ -0,0 +1,17 @@
+﻿using APIworkshop.src.main.rest.dto;
+using APIworkshop.src.main.service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIworkshop.src.main.controller
+{
+    internal class BodyRequestController
+    {
+        private BodyRequestService service = new BodyRequestService();
+
+        public BodyExample newBodyRequest(string name, string lastName, int age) { return this.service.createBodyRequest(name, lastName, age); }
+    }
+}
---
+++ APi-wprkshop/src/main/rest/dto/BodyExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIworkshop.src.main.rest.dto
+{
+    internal class BodyExample
+    {
+        private string name { set; get; }
+        private string lastName { get; set; }
+        private int age { set; get; }
+
+        public string Name{ get { return name; } set { name = value; } }
+        public string LastName { get { return lastName;} set { lastName = value; } }
+        public int Age { get { return age;} set {age= value;} }
+    }
+}
