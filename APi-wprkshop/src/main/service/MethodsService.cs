c46329df7a6e6f0657d7f5c52e140d6f30ca3067
estebanpalomares <79463082+estebanpalomares@users.noreply.github.com>
2023-03-14 15:11:45 -0500

    Api scenarios (#2)
    * Methods Scenarios implemented
    
    * Update gitignore file
    
    ---------
    
    Co-authored-by: Jaime Palomares <jaime.palomares+DFIN@dfinsolutions.com>

---
+++ APi-wprkshop/src/main/service/MethodsService.cs
@@ -0,0 +1,50 @@
+﻿using APIworkshop.src.main.rest.dto;
+using APIworkshop.src.main.rest.request;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIworkshop.src.main.service
+{
+    internal class MethodsService
+    {
+        private MethodRequest request = new MethodRequest();
+
+        public RestResponse useMethod(string method, BodyExample dto)
+        {
+            RestResponse response = null;
+
+            switch(method)
+            {
+                case "GET":
+                    response = this.request.getMethod(dto);
+                    break;
+
+                case "POST":
+                    response = this.request.postMethod(dto);
+                    break;
+
+                case "PUT":
+                    response = this.request.putMethod(dto);
+                    break;
+
+                case "PATCH":
+                    response = this.request.patchMethod(dto);
+                    break;
+
+                case "DELETE":
+                    response = this.request.deleteMethod(dto);
+                    break;
+
+                default:
+                    Assert.NotNull(response);
+                    break;
+            }
+
+            return response;
+        }
+    }
+}
